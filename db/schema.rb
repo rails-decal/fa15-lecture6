38562c6a76166f0bdd1aa3d0dde0c7912c66a263
Sam Lau <samlau95@gmail.com>
2015-10-14 23:22:53 -0700

    Update footer a bit

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20141015022422) do
 
-  create_table "pokemons", force: true do |t|
+  create_table "pokemons", force: :cascade do |t|
     t.string   "name"
     t.integer  "level"
     t.text     "description"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20141015022422) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.integer  "num_badges"
     t.datetime "created_at"
